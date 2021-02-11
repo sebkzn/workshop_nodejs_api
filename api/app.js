d10c52ce9a7ea579c3f6023b8fb88bca5e14d0f2
Sebastien Raoult <sebastien.raoult@epitech.eu>
2021-02-11 02:57:11 +0100

    find all products fixed and GET route to find individual product by id parameter created

--- api/app.js
+++ api/app.js
@@ -34,9 +34,15 @@ app.post('/api/products', (req, res, next) => {
         .catch(error => res.status(400).json({ error }));
 });
 
+app.get('/api/products/:id', (req, res, next) => {
+    Product.findOne({ _id: req.params.id })
+        .then(productFound => res.status(200).json({ product: productFound}))
+        .catch(error => res.status(404).json({ error }));
+});
+
 app.use('/api/products', (req, res, next) => {
     Product.find()
-        .then(products => res.status(200).json(products))
+        .then(allProducts => res.status(200).json({ products: allProducts }))
         .catch(error => res.status(400).json({ error }));
 });
 
