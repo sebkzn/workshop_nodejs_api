9ab19ff2d4ab97501965f88480bbf4dc3d9494cb
Sebastien Raoult <sebastien.raoult@epitech.eu>
2021-02-11 03:10:09 +0100

    PUT route added to update product's informations

--- api/app.js
+++ api/app.js
@@ -40,6 +40,12 @@ app.get('/api/products/:id', (req, res, next) => {
         .catch(error => res.status(404).json({ error }));
 });
 
+app.put('/api/products/:id', (req, res, next) => {
+    Product.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+        .then(() => res.status(201).json({ message: 'Modified!'}))
+        .catch(error => res.status(400).json({ error }));
+});
+
 app.use('/api/products', (req, res, next) => {
     Product.find()
         .then(allProducts => res.status(200).json({ products: allProducts }))
